4b9ebc59f2a76d410970136e8799e469e08de0e6
FireBrandMint <guilhermesouza-2011@hotmail.com>
2025-05-26 18:48:04 -0300

    Criação das tabelas.

---
+++ src/db/initDB.js
@@ -0,0 +1,50 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database.db", sqlite3.OPEN_READWRITE, (err) => {
+    if(err) return console.error(err.message);
+
+    console.log("Connection successful");
+});
+
+init_users();
+init_books();
+init_rent();
+
+function init_users()
+{
+    try
+    {
+        db.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(128) NOT NULL, email varchar(64) NOT NULL, cpf varchar(11), senha INTEGER)")
+    }
+    catch(err)
+    {
+        console.log("ERROR INITIATING USERS!");
+        console.log(err);
+    }
+}
+
+function init_books()
+{
+    try
+    {
+        db.run("CREATE TABLE IF NOT EXISTS livro(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo varchar(128) NOT NULL, categoria varchar(64) NOT NULL)")
+    }
+    catch(err)
+    {
+        console.log("ERROR INITIATING BOOKS!");
+        console.log(err);
+    }
+}
+
+function init_rent()
+{
+    try
+    {
+        db.run("CREATE TABLE IF NOT EXISTS livro_emprestado(id_usuario INTEGER NOT NULL, id_livro INTEGER NOT NULL, data_emprestimo INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999), data_entrega INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999))")
+    }
+    catch(err)
+    {
+        console.log("ERROR INITIATING RENTS!");
+        console.log(err);
+    }
+}
