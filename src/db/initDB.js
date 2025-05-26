545141d301c07946273e8334f54d5dea065bb6d5
FireBrandMint <guilhermesouza-2011@hotmail.com>
2025-05-26 19:23:27 -0300

    List all existing tables upon completion.

--- src/db/initDB.js
+++ src/db/initDB.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./db/database.db", sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database("./src/db/database.db", sqlite3.OPEN_READWRITE, (err) => {
     if(err) return console.error(err.message);
 
     console.log("Connection successful");
@@ -15,7 +15,7 @@ function init_users()
     try
     {
         db.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(128) NOT NULL, email varchar(64) NOT NULL, cpf varchar(11), senha INTEGER)");
-        console.log("Initialized/had users");
+        console.log("Initialized or had users");
     }
     catch(err)
     {
@@ -29,7 +29,7 @@ function init_books()
     try
     {
         db.run("CREATE TABLE IF NOT EXISTS livro(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo varchar(128) NOT NULL, categoria varchar(64) NOT NULL)")
-        console.log("Initialized/had books");
+        console.log("Initialized or had books");
     }
     catch(err)
     {
@@ -43,7 +43,7 @@ function init_rent()
     try
     {
         db.run("CREATE TABLE IF NOT EXISTS livro_emprestado(id_usuario INTEGER NOT NULL, id_livro INTEGER NOT NULL, data_emprestimo INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999), data_entrega INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999))")
-        console.log("Initialized/had rents");
+        console.log("Initialized or had rents");
     }
     catch(err)
     {
@@ -51,3 +51,13 @@ function init_rent()
         console.log(err);
     }
 }
+
+db.all("SELECT tbl_name FROM sqlite_master WHERE type = \'table\'", (err, res) => {
+    if(err)
+        return console.log(err);
+
+    console.log("\nEXISTING TABLES ARRAY");
+    console.log(res);
+    console.log();
+});
+
