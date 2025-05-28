0df990f238d734fb2bdb5bc934baf2eb6e822481
FireBrandMint <guilhermesouza-2011@hotmail.com>
2025-05-27 22:29:41 -0300

    Knex funciona, a variavel 'db' direto do initDB tá funcionando.

--- src/db/initDB.js
+++ src/db/initDB.js
@@ -1,63 +1,149 @@
+const { default: knex } = require("knex");
+
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./src/db/database.db", sqlite3.OPEN_READWRITE, (err) => {
+const db_initializer = new sqlite3.Database("./src/db/database.db", sqlite3.OPEN_READWRITE, (err) => {
     if(err) return console.error(err.message);
 
     console.log("Connection successful");
 });
 
-init_users();
-init_books();
-init_rent();
+let table_initializer = init_all();
+
+function init_all()
+{
+    return new Promise(async (resolve, reject) => 
+    {
+        let fodder = await Promise.all(
+            [
+                init_users(),
+                init_books(),
+                init_rent()
+            ]
+        );
+
+        resolve();
+    })
+}
 
 function init_users()
 {
-    try
+    return new Promise((resolve, reject) => 
     {
-        db.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(128) NOT NULL, email varchar(64) NOT NULL, cpf varchar(11), senha INTEGER)");
-        console.log("Initialized or had users");
-    }
-    catch(err)
-    {
-        console.log("ERROR INITIATING USERS!");
-        console.log(err);
-    }
+        db_initializer.run("CREATE TABLE IF NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(128) NOT NULL, email varchar(64) NOT NULL, cpf varchar(11), senha INTEGER)",
+            (res, err) =>
+            {
+                if(err)
+                {
+                    console.log("Error initializing users.")
+                    console.log(err);
+                }
+                else
+                {
+                    console.log("Initialized or had users.");
+                }
+
+                resolve();
+            }
+        );
+    });
 }
 
 function init_books()
 {
-    try
+    return new Promise((resolve, reject) => 
     {
-        db.run("CREATE TABLE IF NOT EXISTS livro(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo varchar(128) NOT NULL, categoria varchar(64) NOT NULL)")
-        console.log("Initialized or had books");
-    }
-    catch(err)
-    {
-        console.log("ERROR INITIATING BOOKS!");
-        console.log(err);
-    }
+        db_initializer.run("CREATE TABLE IF NOT EXISTS livro(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titulo varchar(128) NOT NULL, categoria varchar(64) NOT NULL)",
+            (res, err) =>
+            {
+                if(err)
+                {
+                    console.log("Error initializing books.")
+                    console.log(err);
+                }
+                else
+                {
+                    console.log("Initialized or had books.");
+                }
+
+                resolve();
+            }
+        );
+    });
 }
 
 function init_rent()
 {
-    try
+    return new Promise((resolve, reject) => 
     {
-        db.run("CREATE TABLE IF NOT EXISTS livro_emprestado(id_usuario INTEGER NOT NULL, id_livro INTEGER NOT NULL, data_emprestimo INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999), data_entrega INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999))")
-        console.log("Initialized or had rents");
-    }
-    catch(err)
-    {
-        console.log("ERROR INITIATING RENTS!");
-        console.log(err);
-    }
+        db_initializer.run("CREATE TABLE IF NOT EXISTS livro_emprestado(id_usuario INTEGER NOT NULL, id_livro INTEGER NOT NULL, data_emprestimo INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999), data_entrega INTEGER NOT NULL CHECK(data_emprestimo > 10000000) CHECK(data_emprestimo < 99999999))",
+            (res, err) =>
+            {
+                if(err)
+                {
+                    console.log("Error initializing rents.")
+                    console.log(err);
+                }
+                else
+                {
+                    console.log("Initialized or had rents.");
+                }
+
+                resolve();
+            }
+        );
+    });
 }
 
-db.all("SELECT tbl_name FROM sqlite_master WHERE type = \'table\'", (err, res) => {
+/*db_initializer.all("SELECT tbl_name FROM sqlite_master WHERE type = \'table\'", (err, res) => {
     if(err)
+    {
+        final_lock = true;
         return console.log(err);
+    }
 
     console.log("\nEXISTING TABLES ARRAY");
     console.log(res);
     console.log();
-});
+    final_lock = true;
+});*/
 
+const db = knex(
+    {
+        client: 'sqlite3',
+        connection: {
+            filename: __dirname + '/database.db'
+        }
+    }
+)
+
+function test_database(result)
+{
+    db.select('tbl_name').from('sqlite_master').where({type: 'table'}).then(
+    (values) =>{
+        console.log("KNEX IS WORKING")
+        console.log(values);
+    }).catch((err) => {
+        console.log("knex error");
+        console.log(err);
+    })
+}
+
+table_initializer.then(() => {
+    let lock = true;
+
+    db_initializer.close((err) =>
+    {
+        if(err)
+        {
+            console.warn("Error closing initializer.");
+            console.log(err);
+            lock = false;
+            return;
+        }
+        
+        lock = false;
+        console.log("Successfully closed initializer.");
+    });
+})
+.then(test_database)
