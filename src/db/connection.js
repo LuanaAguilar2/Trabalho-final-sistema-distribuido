f8999029599c16fee9b7f4c972ca7c4e94209aaa
FireBrandMint <guilhermesouza-2011@hotmail.com>
2025-05-27 22:44:05 -0300

    The connection script was made. Go ahead.

---
+++ src/db/connection.js
@@ -0,0 +1,3 @@
+const initializer = require('./initDB')
+
+const db = initializer.db;
--- src/db/initDB.js
+++ src/db/initDB.js
@@ -19,6 +19,8 @@ const db = knex(
     }
 )
 
+exports.db = db;
+
 table_initializer.then(() => {
     let lock = true;
 
